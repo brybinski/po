b5a68b5f9fa66a2e9b085f196ae347bc7fff029a
Bartosz Rybiński <bartoszrybinski@outlook.com>
2021-10-10 17:33:28 +0200

    Start of work on Lab00

--- Lab01/Lab01.java
+++ Lab01/Lab01.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab01;
 import java.util.Scanner;
 
@@ -10,27 +6,27 @@ import java.util.Scanner;
  *
  * @author local
  */
-public class Lab01 {
+public class Main {
     int nr;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         System.out.println("Count: ");
         Scanner input = new Scanner(System.in);
         int n = Integer.parseInt(input.nextLine());
-        
+
         float[] arr = new float[n];
-                
-        
+
+
         for(int i = 0; i<n; i++){
-            float tmp = 0;
+            float tmp1;
             int where = i+1;
             System.out.println("Number " + where  + ": ");
             Scanner dfinput = new Scanner(System.in);
-            tmp = Float.parseFloat(dfinput.nextLine());
-            arr[i] = tmp;
+            tmp1 = Float.parseFloat(dfinput.nextLine());
+            arr[i] = tmp1;
         }
 
         //a
@@ -39,8 +35,8 @@ public class Lab01 {
             a += arr[i];
         }
         System.out.println("a) " + a);
-        
-        
+
+
         //b
         float b = 1;
         for (int i = 0; i < n; i++) {
@@ -54,22 +50,22 @@ public class Lab01 {
             c += Math.abs(arr[i]);
         }
         System.out.println("c) " + c);
-        
+
         //d
         float d = 0;
         for (int i = 0; i < n; i++) {
             d += Math.sqrt(Math.abs(arr[i]));
         }
         System.out.println("d) " + d);
-        
+
         //e
         float e = 1;
         for (int i = 0; i < n; i++) {
             e *= Math.abs(arr[i]);
         }
         System.out.println("e) " + e);
-        
-        
+
+
         //f
         float f = 0;
         for (int i = 0; i < n; i++) {
@@ -77,5 +73,5 @@ public class Lab01 {
         }
         System.out.println("f) " + f);
     }
-    
+
 }
---
+++ src/Lab00/Zadanie2.java
@@ -0,0 +1,10 @@
+package Lab00;
+
+public class Zadanie2 {
+    public static void main(String[] args){
+
+        int x = 10;
+
+        System.out.println(((x+1)*x)/2);
+    }
+}
