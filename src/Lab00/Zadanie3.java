0f7fbf8f50d053a63cdc083921ea42075f540d9b
Bartosz Rybiński <bartoszrybinski@outlook.com>
2021-10-10 18:23:54 +0200

    Coffee break

---
+++ src/Lab00/Zadanie3.java
@@ -0,0 +1,15 @@
+package Lab00;
+
+public class Zadanie3 {
+    public static void main(String[] args){
+        int x = 10;
+        int result = 1;
+        while (x != 0){
+            result *= x;
+            x--;
+        }
+        System.out.println(result);
+    }
+}
+
+
